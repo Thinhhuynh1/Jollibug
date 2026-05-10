015119d507378d1f22b8feedb4ef2dd0ea752773
unknown <thinhhuynh.dk2006@gmail.com>
2026-05-10 22:40:47 +0700

    feature/manager-product

--- src/main/java/vn/fastfood/config/WebMvcConfig.java
+++ src/main/java/vn/fastfood/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package vn.fastfood.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,6 +15,11 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Bean(name = "multipartResolver")
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
     @Bean
     public ViewResolver viewResolver() {
         final InternalResourceViewResolver bean = new InternalResourceViewResolver(); // là class chuyên dùng cho JSP
