a32f04f1cedb7548b82efa4a3e4ad3c579a86e4f
LawTx06 <nguyenbathien035358@gmail.com>
2026-05-29 20:54:44 +0700

    Update DBConnection.java

--- src/main/java/vn/fastfood/config/DBConnection.java
+++ src/main/java/vn/fastfood/config/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1522:orclpdb";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1522/orclpdb";
     private static final String USERNAME = "fastfooddb";
     private static final String PASSWORD = "123456";
 
